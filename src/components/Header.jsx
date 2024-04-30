72021c01d642895af34035c4ab3fcf47da160854
116495254+MIbrahim71@users.noreply.github.com <Ibrahim_M789@outlook.com>
2024-04-30 21:37:32 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Budgets</h1>
+      <div className="header-btns">
+        <button>Add Budget</button>
+        <button>Add Expense</button>
+      </div>
+    </div>
+  );
+}
