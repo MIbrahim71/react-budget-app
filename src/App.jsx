f823bd6d1dbd96774abee0f36a03230c92ed7c8d
116495254+MIbrahim71@users.noreply.github.com <Ibrahim_M789@outlook.com>
2024-05-01 00:39:36 +0100

    budget card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import BudgetCard from "./components/BudgetCard";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <div className="budget-container">
+        <BudgetCard />
+      </div>
     </>
   );
 }
