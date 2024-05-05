e011425989f71a5335aab57a9c2278b58ca0124a
116495254+MIbrahim71@users.noreply.github.com <Ibrahim_M789@outlook.com>
2024-05-05 16:00:47 +0100

    add budgets

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import AddBudgetModal from "./components/AddBudgetModal";
 import BudgetCard from "./components/BudgetCard";
 import Header from "./components/Header";
 import { useState } from "react";
+import { BudgetsProvider, useBudgets } from "./contexts/BudgetsContext";
 
 function App() {
   const [modalOpen, setModalOpen] = useState(false);
+  const { budgets, getBudgetExpenses } = useBudgets();
 
   const handleOpenModal = () => {
     setModalOpen(true);
@@ -18,8 +20,21 @@ function App() {
     <>
       <Header onOpenModal={handleOpenModal} />
       <div className="budget-container">
-        <BudgetCard name="Entertainment" amount={300} max={1000} />
-        <BudgetCard name="Rent" amount={900} max={1000} />
+        {budgets.map((budget) => {
+          // Get all expenses, add them all together and assign to amount variable
+          const amount = getBudgetExpenses(budget.id).reduce(
+            (total, expense) => total + expense.amount,
+            0
+          );
+          return (
+            <BudgetCard
+              key={budget.id}
+              name={budget.name}
+              amount={amount}
+              max={budget.max}
+            />
+          );
+        })}
         <AddBudgetModal isOpen={modalOpen} onClose={handleCloseModal} />
       </div>
     </>
