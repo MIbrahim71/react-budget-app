40e9c8ea13ae3b1a026842cf57c0a85a650ca489
116495254+MIbrahim71@users.noreply.github.com <Ibrahim_M789@outlook.com>
2024-05-21 19:18:34 +0100

    ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
+import { useBudgets } from "../contexts/BudgetsContext";
+
 export default function Header({ onOpenBudgetModal, onOpenExpenseModal }) {
+  const { budgets } = useBudgets();
   return (
     <div className="header">
       <h1>Budgets</h1>
       <div className="header-btns">
         <button onClick={onOpenBudgetModal}>Add Budget</button>
-        <button onClick={onOpenExpenseModal}>Add Expense</button>
+        {budgets.length > 0 && (
+          <button onClick={onOpenExpenseModal}>Add Expense</button>
+        )}
       </div>
     </div>
   );
