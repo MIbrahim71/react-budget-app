0e42703642106614e7dbab0d8a80e346143ba5ec
116495254+MIbrahim71@users.noreply.github.com <Ibrahim_M789@outlook.com>
2024-05-21 22:44:40 +0100

    -

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BudgetsProvider } from "./contexts/BudgetsContext";
+import { BudgetsProvider } from "./contexts/BudgetsContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
